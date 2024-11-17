d93618164d42a2efefa826ce6cb4f0aacf006b07
LinMihigo <linmihigo@gmail.com>
2024-11-17 18:24:00 +0200

    task 11 to 14

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models import storage
+import re
 
 
 class HBNBCommand(cmd.Cmd):
@@ -95,6 +96,7 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, line):
         """
         *** Usage: all <ClassName>
+            or: <ClassName>.all()
         Prints all string repr. of all instances based on or not on the class
         name
 
@@ -111,6 +113,30 @@ class HBNBCommand(cmd.Cmd):
             if args[0] in globals() and isinstance(globals()[args[0]], type):
                 print([str(val)
                        for val in values if val.__class__.__name__ == args[0]])
+            else:
+                print("** class doesn't exit **")
+
+    def do_count(self, line):
+        """
+        *** Usage: count <ClassName>
+            or: <ClassName>.count() (Preferred)
+        Counts number of instances of a class
+
+        Args:
+            line (str): string containing name of class
+        """
+        args = line.split()
+        all_objs = storage.all()
+        val = list(all_objs.values())
+
+        if len(args) == 0:
+            print(f"{len(val)}")
+        else:
+            if args[0] in globals() and isinstance(globals()[args[0]], type):
+                length = len([v
+                              for v in val if v.__class__.__name__ == args[0]
+                              ])
+                print(f"{length}")
             else:
                 print("** class doesn't exit **")
 
@@ -179,6 +205,38 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Disables repetition of previous command on newline"""
         pass
+
+    def default(self, line):
+        """Handles unconventional command formats"""
+        if "." in line:
+            str = re.match(r"(\w+)\.(.+)", line)
+            class_name, command = str.group(1), str.group(2).strip()
+
+            if command == "all()":
+                self.do_all(class_name)
+            elif command == "count()":
+                self.do_count(class_name)
+            elif command[:5] == "show(":
+                match = re.match(r"show\(\"(.*?)\"\)", command)
+                args = list(match.groups()) if match else None
+                if args:
+                    line = " ".join([class_name, args[0]])
+                else:
+                    line = class_name
+                self.do_show(line)
+            elif command[:8] == "destroy(":
+                match = re.match(r"destroy\(\"(.*?)\"\)", command)
+                if match:
+                    args = list(match.groups())
+                if args:
+                    line = " ".join([class_name, args[0]])
+                else:
+                    line = class_name
+                self.do_destroy(line)
+            else:
+                print(f"Unknown syntax: {line}")
+        else:
+            print(f"Unknown syntax: {line}")
 
 
 if __name__ == '__main__':
